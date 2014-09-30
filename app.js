b841dd097c2802ad95b0ebb1098c0fa575e87c44
Liviu Ignat <iviuignat@TheSource.(none)>
2014-09-30 15:47:49 +0200

    Add sticky session module support

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var Server = require('./app/server').Server;
 var cluster = require('cluster');
 var os = require('os');
+var sticky = require('sticky-session');
 
 var numCPUs = os.cpus().length;
 console.log('CPUS : ' + numCPUs);
 
 if (cluster.isMaster) {
-  var server = new Server({
-      dirName: __dirname,
-      enableSocket: true
-    })
-    .setupApp()
-    .setupRoutes()
-    .start();
+  sticky(function() {
+    var server = new Server({
+        dirName: __dirname,
+        enableSocket: true
+      })
+      .setupApp()
+      .setupRoutes();
+    return server.http;
+  }).listen(3000, function() {
+    console.log('server started on 3000 port');
+  });
 
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
@@ -22,11 +27,15 @@ if (cluster.isMaster) {
     console.log('worker ' + worker.process.pid + ' died');
   });
 } else {
-  var server = new Server({
-      dirName: __dirname,
-      enableSocket: true
-    })
-    .setupApp()
-    .setupRoutes()
-    .start();
+  sticky(function() {
+    var server = new Server({
+        dirName: __dirname,
+        enableSocket: true
+      })
+      .setupApp()
+      .setupRoutes();
+    return server.http;
+  }).listen(3000, function() {
+    console.log('server started on 3000 port');
+  });
 }
